c910daae3e592c6320b3ed87be2abfc954ca21e0
Girish Suthar <girishgajjar2003@gmail.com>
2026-06-08 17:47:12 +0530

    update and handle the fail time pipline run

--- vars/runPlaywrightTests.groovy
+++ vars/runPlaywrightTests.groovy
@@ -1,7 +1,18 @@
 def call() {
-    if (isUnix()) {
-        sh 'npx playwright test'
-    } else {
-        bat 'npx playwright test'
+    try {
+        echo "🧪 Running Playwright tests..."
+
+        if (isUnix()) {
+            sh 'npx playwright test'
+        } else {
+            bat 'npx playwright test'
+        }
+
+    } catch (err) {
+        echo "⚠️ Playwright tests failed but continuing pipeline..."
+        currentBuild.result = 'UNSTABLE'
+
+    } finally {
+        echo "📄 Test execution completed. Reports should be generated (playwright-report/)"
     }
 }
